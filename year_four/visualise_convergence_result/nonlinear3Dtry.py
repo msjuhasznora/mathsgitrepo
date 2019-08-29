fc33f41f61deb13aac74f80a9620c71c12c712e9
Nora Juhasz <ms.juhasz.nora@gmail.com>
2019-08-29 10:54:04 +0200

    some semi-working versions

---
+++ year_four/visualise_convergence_result/nonlinear3Dtry.py
@@ -0,0 +1,57 @@
+# nonlinear solver, boundary integral included, the Jacobian is computed automatically.
+# gmres solver
+# stationary, div u WF missing
+
+import matplotlib.pyplot as plt
+from dolfin import *
+import numpy as np
+import datetime
+from numpy.random import rand
+
+from dolfin import *
+import numpy
+
+# Create mesh and define function space
+mesh = UnitCubeMesh(20, 20, 20)
+V = VectorFunctionSpace(mesh, "Lagrange", 2, dim = 3)
+
+# Define boundary conditions
+class UpperBoundary(SubDomain):
+    def inside(self, x, on_boundary):
+        return near(x[2], 1.0)
+upperboundary = UpperBoundary()
+boundaries = MeshFunction("size_t", mesh, mesh.topology().dim() - 1)
+boundaries.set_all(0)
+upperboundary.mark(boundaries, 1)
+ds = Measure('ds')[boundaries]
+
+noslipbasin = DirichletBC(V, (0, 0, 0), "on_boundary && x[2] < 1.0 - DOLFIN_EPS")
+zerotop = DirichletBC(V.sub(2), 0, "on_boundary && x[2] > 1.0 - DOLFIN_EPS")
+
+bcu = [noslipbasin, zerotop]
+
+wind_shear_x = 10.0
+wind_shear_y = 10.0
+theta = Constant((wind_shear_x, wind_shear_y, 0))
+
+# Choice of nonlinear coefficient
+
+c1 = 1.0
+c2 = 1.0
+c3 = 0.000001
+
+# Define variational problem
+v = TestFunction(V)
+u = Function(V)
+u1, u2, u3 = split(u)
+v1, v2, v3 = split(v)
+F = c1 * inner(u, grad(u1)) * v1 * dx + c2 * inner(u, grad(u2)) * v2 * dx + c3 * c3 * inner(u, grad(u3)) * v3 * dx + \
+inner(grad(u1),grad(v1)) * dx + inner(grad(u2),grad(v2)) * dx + c3 * c3 * inner(grad(u3),grad(v3)) * dx + \
+- inner(theta, v) * ds(1)
+
+# Compute solution
+solve(F == 0, u, bcu,
+      solver_parameters = {"newton_solver":{ "linear_solver" : "gmres"}})
+
+file = File("AJ_nonlinear_u.pvd")
+file << u
