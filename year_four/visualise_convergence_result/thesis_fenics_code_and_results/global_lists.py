b459eedcd5b79236d1df757d8ab0fea563075f50
Nora Juhasz <ms.juhasz.nora@gmail.com>
2020-01-09 22:46:59 +0100

    ?

--- year_four/visualise_convergence_result/thesis_fenics_code_and_results/global_lists.py
+++ year_four/visualise_convergence_result/thesis_fenics_code_and_results/global_lists.py
@@ -1,46 +1,44 @@
-def init_anis_hydr_lists():
+def global_lists_init():
 
     global anisotropic_norm_u1_values
     anisotropic_norm_u1_values = []
-    
+
     global anisotropic_norm_u3_values
     anisotropic_norm_u3_values = []
-    
+
     global anisotropic_norm_p_values
     anisotropic_norm_p_values = []
-    
+
     global anisotropic_norm_c_values
     anisotropic_norm_c_values = []
 
     global anisotropic_interpolated_norm_u1_values
     anisotropic_interpolated_norm_u1_values = []
-    
+
     global anisotropic_interpolated_norm_u3_values
     anisotropic_interpolated_norm_u3_values = []
-    
+
     global anisotropic_interpolated_norm_p_values
     anisotropic_interpolated_norm_p_values = []
 
     global interpolated_and_hydr_difference_norm_u1_values
     interpolated_and_hydr_difference_norm_u1_values = []
-    
+
     global interpolated_and_hydr_difference_norm_u3_values
     interpolated_and_hydr_difference_norm_u3_values = []
-    
+
     global interpolated_and_hydr_difference_norm_p_values
     interpolated_and_hydr_difference_norm_p_values = []
 
     global anis_and_hydr_difference_norm_u1_values
     anis_and_hydr_difference_norm_u1_values = []
-    
+
     global anis_and_hydr_difference_norm_p_values
     anis_and_hydr_difference_norm_p_values = []
-    
+
     global anis_and_hydr_difference_norm_c_values
     anis_and_hydr_difference_norm_c_values = []
 
-
-def init_error_lists():
 
     global log_errorvalues_L2_u1
     log_errorvalues_L2_u1 = []
@@ -50,7 +48,7 @@ def init_error_lists():
     log_errorvalues_L2_p = []
     global log_errorvalues_L2_c
     log_errorvalues_L2_c = []
-    
+
     global log_errorvalues_H1_u1
     log_errorvalues_H1_u1 = []
     global log_errorvalues_H1_u3
@@ -59,7 +57,7 @@ def init_error_lists():
     log_errorvalues_H1_p = []
     global log_errorvalues_H1_c
     log_errorvalues_H1_c = []
-    
+
     global errorvalues_H1_u1
     errorvalues_H1_u1 = []
     global errorvalues_H1_u3
@@ -68,7 +66,7 @@ def init_error_lists():
     errorvalues_H1_p = []
     global errorvalues_H1_c
     errorvalues_H1_c = []
-    
+
     global eocvalues_L2_u1
     eocvalues_L2_u1 = []
     global eocvalues_L2_u3
@@ -77,7 +75,7 @@ def init_error_lists():
     eocvalues_L2_p = []
     global eocvalues_L2_c
     eocvalues_L2_c = []
-    
+
     global nxvalues
     nxvalues = []
     global errorvalues
