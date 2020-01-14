1e37dea00445c18aa727306358c811399f4106c8
Nora Juhasz <ms.juhasz.nora@gmail.com>
2020-01-14 16:35:51 +0100

    on apply bcs

--- year_four/visualise_convergence_result/thesis_fenics_code_and_results/comments.py
+++ year_four/visualise_convergence_result/thesis_fenics_code_and_results/comments.py
@@ -1,4 +1,4 @@
-# apply_bcc.py: these functions are necessary because at the moment of definition of the problem_data objects, we don't yet know the mesh (as it is changing). So each time we need to apply the BCs when the mesh is known in the given loop.
+# apply_bcc.py: these functions are necessary because at the moment of definition of the problem_data objects, we don't yet know the mesh (as it is changing). So each time we need to apply the BCs when the mesh is known in the given loop. a technical interest: while the BCs itself remain the same within a problem, they have to be redefined as the function space changes, since the mesh changes.
 
 # *** --- *** --- *** --- *** --- *** --- *** --- *** --- *** --- *** --- *** --- *** --- *** --- *** --- *** --- #
 
